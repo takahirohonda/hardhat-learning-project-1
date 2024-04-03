08042d20fb6f964ae0f32ef59f466cb6731fe42f
mydatahack <thon6thon6@hotmail.com>
2024-04-03 16:25:10 +1100

    Update deploy script

--- scripts/deployHello.ts
+++ scripts/deployHello.ts
@@ -1,5 +1,7 @@
 
 // import "@nomiclabs/hardhat-ethers" -> the instruction tells us to import, but we haven't installed it. For now, not bothering it.
+import "@nomicfoundation/hardhat-toolbox"; // see https://hardhat.org/hardhat-runner/docs/advanced/migrating-from-hardhat-waffle
+
 import { ethers } from 'hardhat'
 import { HelloWorld } from "../typechain-types"
 
@@ -8,6 +10,9 @@ const deploy = async () => {
   const HelloWorld = await ethers.getContractFactory("HelloWorld")
   const hello = await HelloWorld.deploy()
   // await hello.deployed() -> the instruction has this line but deployed() doesn't exist...
+  // deployed() is deprecated for hardhat-toolbox. The course is using hardhat-waffle, but it is now moved to hardhat-toolbox.
+  // https://hardhat.org/hardhat-runner/docs/advanced/migrating-from-hardhat-waffle
+  await hello.waitForDeployment() 
   return hello
 }
 
