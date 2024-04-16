2516faea44df9341cb85e1c48b9b5cdc23aee5b6
mydatahack <43569574+mydatahack@users.noreply.github.com>
2024-04-16 13:24:53 +1000

    Fix counter error (#2)
    * Fix counter
    
    * Update

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -6,13 +6,23 @@ import "hardhat/console.sol";
 
 contract Counter {
   uint private counter;
-  function count() public returns (uint) {
+  // This doesn't work. We need write and read separate
+  // You cannot read with write function because it takes time to transaction to complete.
+  // function count() public returns (uint) {
+  //   counter++;
+  //   console.log('counter', counter);
+  //   return counter;
+  // }
+
+  function count() public {
     counter++;
-    console.log('counter', counter);
-    return counter;
+    console.log('counter is now: ', counter);
   }
 
-  function getCounter () public view returns (uint) {
+  // unit is big number in js, unit 32 is just treated by a regular number in js.
+  function getCounter() public view returns (uint) {
     return counter;
+    // can cast but you'll run into a danger of overflow.
+    // return unit32(counter); 
   }
 }
