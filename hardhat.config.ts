09836e3d13fd118b7c0940c0e93b98b0415ce69a
mydatahack <thon6thon6@hotmail.com>
2024-04-18 07:14:44 +1000

    Add new contract with tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,12 @@ import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false
+  },
   networks: {
     hardhat: {
-      chainId: 31337, 
+      chainId: 31337, // it's the default chain id, doesn't need to add it.
     }
   }
 };
